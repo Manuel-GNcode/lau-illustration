21acc4445ba2353e98292875225ce99fbc454601
GNcode <gngalarga027@gmail.com>
2025-10-21 16:03:14 -0500

    Update favicon, enhance animations, and improve cursor styles

--- src/home/animationHome.js
+++ src/home/animationHome.js
@@ -1,6 +1,7 @@
 import { gsap } from "gsap";
 
 const $homeBtn = document.getElementById('home-btn');
+const $flowerFall = document.querySelectorAll('.flower-fall');
 
 // Animación del avatar del home
 gsap.to('#home-avatar', {
@@ -40,3 +41,31 @@ tlHome.to('#home-btn', {
 $homeBtn.addEventListener('click', e=>{
   tlHome.restart();
 })
+
+// Animación caida de flores
+
+const flowerFall = (flower)=>{
+  const xMove = gsap.utils.random(-150, 150);
+  const flowerDelay = gsap.utils.random(0, 6);
+
+  gsap.to(flower, {
+    y: '110dvh',
+    x: xMove,
+    rotate: 180,
+    duration: 5,
+    delay: flowerDelay,
+    ease: 'sine.in',
+    onComplete: ()=>{
+      gsap.set(flower, {
+        x:0,
+        y:0,
+        rotate: 0,
+      })
+      flowerFall(flower);
+    }
+  })
+}
+$flowerFall.forEach(el=>{
+  flowerFall(el);
+})
+
